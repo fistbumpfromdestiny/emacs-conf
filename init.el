32a75fbc5847e8327509a8bbf2883a8d33dea29e
Fredrik Larsson <fredrik@wizardry.se>
2022-07-09 15:47:15 +0200

    Added Emacs.org to handle configuration through Babel

--- init.el
+++ init.el
@@ -3,75 +3,42 @@
 (setq inhibit-startup-message t
       visible-bell t)
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-(setq auth-sources '("~/.authinfo.gpg"))
-
-;; Turn off some unneeded UI elements
+  ;; Turn off some unneeded UI elements
 
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
+(scroll-bar-mode -1)     
 (tooltip-mode -1)
 (menu-bar-mode -1)
 (set-fringe-mode 10)
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height default-font-size)
 
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 ;; Display colymn and line numbers in every buffer
 (column-number-mode)
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
 
-;; Disable them for some modes though.
- (defun do-not-display-line-numbers-mode-hook()
-      "Hook to disable line-number-mode."
-      (display-line-numbers-mode 0))
+  ;; Disable them for some modes though.
+(defun do-not-display-line-numbers-mode-hook()
+  "Hook to disable line-number-mode."
+  (display-line-numbers-mode 0))
 
 (add-hook 'org-mode-hook #'do-not-display-line-numbers-mode-hook)
 (add-hook 'eshell-mode-hook #'do-not-display-line-numbers-mode-hook)
 
-;; Initialize package sources
-(require 'package)
+;; Rainbow Delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
- (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-   (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package doom-themes
+;; Add Which Key to show available key modes
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dracula t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  (setq which-key-idle-delay 0.3))
 
 (use-package ivy
-  ;:diminish
+                                        ;:diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
          ("TAB" . ivy-alt-done)	
@@ -88,84 +55,23 @@
   :config
   (ivy-mode 1))
 
-(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
-
-(use-package swiper)
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil))
-
 (use-package ivy-rich
   :after ivy
   :init
   (ivy-rich-mode 1))
 
-(setq package-selected-packages '(clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 
-(add-hook 'clojure-mode-hook 'lsp)
-(add-hook 'clojurescript-mode-hook 'lsp)
-(add-hook 'clojurec-mode-hook 'lsp)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'java-mode-hook 'lsp)
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-minimum-prefix-length 1
-      lsp-lens-enable t
-      lsp-signature-auto-activate nil
-      ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
-      ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
-      )
-
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-(setq lsp-lens-enable t)
-
-;; Rainbow Delimiters
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; Add Which Key to show available key modes
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
   :config
-  (setq which-key-idle-delay 0.3))
+  (setq ivy-initial-inputs-alist nil))
 
-;; Projectile settings
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/_Dev")
-    (setq projectile-project-search-path '("~/_Dev")))
-  ;; Decides what the first action is whenever you switch between projects; load up dired.
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :after projectile
-  :config (counsel-projectile-mode))
+(use-package swiper)
 
 (use-package helpful
   :commands (helpful-callable helpful-variable helpful-command helpful-key)
@@ -178,7 +84,10 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-(use-package general)
+(defhydra hydra-text-scale (:timeout 4)
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
 
 (use-package evil
   :init
@@ -211,21 +120,52 @@
     :prefix "SPC"
     :global-prefix "C-SPC"))
 
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
 (fl/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(use-package magit)
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package forge
-  :after magit)
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-dracula t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 (defun fl/org-mode-setup ()
   (org-indent-mode)
@@ -238,85 +178,84 @@
   :hook (org-mode . fl/org-mode-setup)
   :config
   (setq org-ellipsis " ▾"
-	org-hide-emphasis-markers t)
+        org-hide-emphasis-markers t)
 
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
-  
+
   (setq org-agenda-files '("~/Orgs/TODO.org")))
 
 
 (setq org-todo-keywords
-   '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-     (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+        (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
 
 (setq org-refile-targets
-   '(("Archive.org" :maxlevel . 1)
-     ("TODO.org" :maxlevel . 1)))
+      '(("Archive.org" :maxlevel . 1)
+        ("TODO.org" :maxlevel . 1)))
 
- ;; Save Org buffers after refiling!
+;; Save Org buffers after refiling!
 (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
 (setq org-tag-alist
-   '((:startgroup)
-    ; Put mutually exclusive tags here
-     (:endgroup)
-     ("@errand" . ?E)
-     ("@home" . ?H)
-     ("@work" . ?W)
-     ("agenda" . ?a)
-     ("planning" . ?p)
-     ("note" . ?n)
-     ("idea" . ?i)))
+      '((:startgroup)
+                                        ; Put mutually exclusive tags here
+        (:endgroup)
+        ("@errand" . ?E)
+        ("@home" . ?H)
+        ("@work" . ?W)
+        ("agenda" . ?a)
+        ("planning" . ?p)
+        ("note" . ?n)
+        ("idea" . ?i)))
 
 ;; Configure custom agenda views
 (setq org-agenda-custom-commands
-  '(("d" "Dashboard"
-    ((agenda "" ((org-deadline-warning-days 7)))
-     (todo "NEXT"
-       ((org-agenda-overriding-header "Next Tasks")))
-     (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+      '(("d" "Dashboard"
+         ((agenda "" ((org-deadline-warning-days 7)))
+          (todo "NEXT"
+                ((org-agenda-overriding-header "Next Tasks")))
+          (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
 
-   ("n" "Next Tasks"
-    ((todo "NEXT"
-       ((org-agenda-overriding-header "Next Tasks")))))
+        ("n" "Next Tasks"
+         ((todo "NEXT"
+                ((org-agenda-overriding-header "Next Tasks")))))
 
-   ("W" "Work Tasks" tags-todo "+work-email")
+        ("W" "Work Tasks" tags-todo "+work-email")
 
-   ;; Low-effort next actions
-   ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-    ((org-agenda-overriding-header "Low Effort Tasks")
-     (org-agenda-max-todos 20)
-     (org-agenda-files org-agenda-files)))
+        ("w" "Workflow Status"
+         ((todo "WAIT"
+                ((org-agenda-overriding-header "Waiting on External")
+                 (org-agenda-files org-agenda-files)))
+          (todo "REVIEW"
+                ((org-agenda-overriding-header "In Review")
+                 (org-agenda-files org-agenda-files)))
+          (todo "PLAN"
+                ((org-agenda-overriding-header "In Planning")
+                 (org-agenda-todo-list-sublevels nil)
+                 (org-agenda-files org-agenda-files)))
+          (todo "BACKLOG"
+                ((org-agenda-overriding-header "Project Backlog")
+                 (org-agenda-todo-list-sublevels nil)
+                 (org-agenda-files org-agenda-files)))
+          (todo "READY"
+                ((org-agenda-overriding-header "Ready for Work")
+                 (org-agenda-files org-agenda-files)))
+          (todo "ACTIVE"
+                ((org-agenda-overriding-header "Active Projects")
+                 (org-agenda-files org-agenda-files)))
+          (todo "COMPLETED"
+                ((org-agenda-overriding-header "Completed Projects")
+                 (org-agenda-files org-agenda-files)))
+          (todo "CANC"
+                ((org-agenda-overriding-header "Cancelled Projects")
+                 (org-agenda-files org-agenda-files)))))))
 
-   ("w" "Workflow Status"
-    ((todo "WAIT"
-           ((org-agenda-overriding-header "Waiting on External")
-            (org-agenda-files org-agenda-files)))
-     (todo "REVIEW"
-           ((org-agenda-overriding-header "In Review")
-            (org-agenda-files org-agenda-files)))
-     (todo "PLAN"
-           ((org-agenda-overriding-header "In Planning")
-            (org-agenda-todo-list-sublevels nil)
-            (org-agenda-files org-agenda-files)))
-     (todo "BACKLOG"
-           ((org-agenda-overriding-header "Project Backlog")
-            (org-agenda-todo-list-sublevels nil)
-            (org-agenda-files org-agenda-files)))
-     (todo "READY"
-           ((org-agenda-overriding-header "Ready for Work")
-            (org-agenda-files org-agenda-files)))
-     (todo "ACTIVE"
-           ((org-agenda-overriding-header "Active Projects")
-            (org-agenda-files org-agenda-files)))
-     (todo "COMPLETED"
-           ((org-agenda-overriding-header "Completed Projects")
-             (org-agenda-files org-agenda-files)))
-     (todo "CANC"
-           ((org-agenda-overriding-header "Cancelled Projects")
-            (org-agenda-files org-agenda-files)))))))
+(setq org-capture-templates
+      `(("t" "Tasks / Projects")
+        ("tt" "Task" entry (file+olp "~/Orgs/TODO.org" "Inbox")
+         "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)))
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
@@ -339,8 +278,8 @@
   (font-lock-add-keywords 'org-mode
                           '(("^ *\\([-]\\) "
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-  
-    ;; Set faces for heading levels
+
+  ;; Set faces for heading levels
   (dolist (face '((org-level-1 . 1.2)
                   (org-level-2 . 1.1)
                   (org-level-3 . 1.05)
@@ -350,3 +289,94 @@
                   (org-level-7 . 1.1)
                   (org-level-8 . 1.1)))
     (set-face-attribute (car face) nil :font "Fira Code Retina" :weight 'regular :height (cdr face))))
+
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (clojure . t)
+     (C . t)
+     (java . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(require 'ob-clojure)
+(setq org-babel-clojure-backend 'cider)
+
+(defun fl/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.emacs.d/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'fl/org-babel-tangle-config)))
+
+(with-eval-after-load 'org
+  ;; This is needed as of Org 9.2
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("cl" . "src clojure"))
+  (add-to-list 'org-structure-template-alist '("cc" . "src C"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python")))
+
+(setq auth-sources '("~/.authinfo.gpg"))
+
+(use-package magit)
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge
+  :after magit)
+
+(use-package lsp-mode
+    :init
+    ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+    (setq lsp-keymap-prefix "C-c l")
+    :hook (
+           ;; if you want which-key integration
+           (lsp-mode . lsp-enable-which-key-integration))
+    :commands lsp)
+
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+(setq lsp-lens-enable t)
+
+(setq package-selected-packages '(clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+
+(add-hook 'clojure-mode-hook 'lsp)
+(add-hook 'clojurescript-mode-hook 'lsp)
+(add-hook 'clojurec-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'java-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-minimum-prefix-length 1
+      lsp-lens-enable t
+      lsp-signature-auto-activate nil
+                                        ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
+                                        ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+      )
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/_Dev")
+    (setq projectile-project-search-path '("~/_Dev")))
+  ;; Decides what the first action is whenever you switch between projects; load up dired.
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :after projectile
+  :config (counsel-projectile-mode))
