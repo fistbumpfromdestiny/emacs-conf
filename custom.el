e38bf63ecb9c88475f53ac4445944b54b894751d
Fredrik Larsson <fredrik@wizardry.se>
2022-07-10 12:32:47 +0200

    Adding depth to Clojure config.

--- custom.el
+++ custom.el
@@ -1,5 +1,13 @@
 (custom-set-variables
- '(package-selected-packages
-   '(visual-fill-column clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lsp-treemacs flycheck))
  '(warning-suppress-types '((lsp-mode) (comp))))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
